6a11973c8a7e68e55a62e3af8c00d606677f4b8b
Oleg <31519602+Jeerjmin@users.noreply.github.com>
2022-09-02 22:49:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World! Its node js app');
+    res.end('Hello World! Its node js app (edited)');
 });
 
 server.listen(port, host, () => {
